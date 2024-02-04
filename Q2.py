dba8a79ddcde6e4e20bdc1759d0f5e568bc2fb9b
Ritesh102000 <ritesh23075@iiitd.ac.in>
2024-02-04 18:02:27 +0530

    Seacond push Q1 complete Q2 incomplete

--- Q2.py
+++ Q2.py
@@ -1,16 +1,40 @@
 import os
+import pickle
+
 def inv_index():
-     dic = {}
-     for filename in os.listdir('output'):
-          with open(f'output/{filename}','r') as file:
-               temp_lst = file.read().split()
-               for i in temp_lst:
-                    if i in dic:
-                         if filename not in dic[i]:
-                              dic[i+'count']+=1
-                              dic[i].append(filename)
+    dic = {}
+    for filename in os.listdir('output'):
+        with open(f'output/{filename}', 'r') as file:
+            temp_lst = file.read().split()
+            for i in temp_lst:
+                if i not in dic:
+                    dic[i] = {}
+                    dic[i][filename] = 1
+                    dic[i]['count'] = 1
+                else:
+                    if filename not in dic[i]:
+                        dic[i][filename] = 1
+                        dic[i]['count'] += 1
                     else:
-                         dic[i]=[filename]
-                         dic[i+'count'] = 1
-     print(dic)
-inv_index()
+                        dic[i][filename] += 1
+                        dic[i]['count'] += 1
+
+    return dic
+
+def save_index(index, filename='inverted_index.pkl'):
+    with open(filename, 'wb') as file:
+        pickle.dump(index, file)
+
+def load_index(filename='inverted_index.pkl'):
+    with open(filename, 'rb') as file:
+        return pickle.load(file)
+
+inverted_index = inv_index()
+
+
+save_index(inverted_index, 'inverted_index.pkl')
+
+
+loaded_index = load_index('inverted_index.pkl')
+
+
