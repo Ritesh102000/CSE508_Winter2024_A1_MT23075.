45fc8c92dc0a137db312a3a2cd70221f2d691cce
Ritesh102000 <ritesh23075@iiitd.ac.in>
2024-02-04 16:59:50 +0530

    First push

---
+++ Q1.py
@@ -0,0 +1,20 @@
+import spacy
+import string
+import os
+from spacy.lang.en.stop_words import STOP_WORDS
+def tokenization(x):
+    filepath = f'C:/Users/pc/Desktop/IR-MT23075/text_files/{x}'
+    with open(filepath,'r') as file:
+        content = file.read()
+        content = content.lower()
+        nlp = spacy.load("en_core_web_sm")
+        doc= nlp(content)
+        tokens = [token.text for token in doc if token.text not in string.punctuation and token.text not in STOP_WORDS and not token.is_space]    
+    return tokens
+
+for filename in os.listdir('text_files'):
+    temp = tokenization(filename)
+    with open(f'output/{filename}','w') as file:
+        file.write(" ".join(temp))
+
+
